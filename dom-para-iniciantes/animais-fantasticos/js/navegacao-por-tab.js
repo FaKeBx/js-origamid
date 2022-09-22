100a83ac18846b68d187ac4797217a691b383b17
Felipe kucharski <felipekb2000@furg.br>
2022-09-22 13:27:25 -0300

    criando scrool suave

--- dom-para-iniciantes/animais-fantasticos/js/navegacao-por-tab.js
+++ dom-para-iniciantes/animais-fantasticos/js/navegacao-por-tab.js
@@ -40,3 +40,25 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initScroolSuave() {
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+  console.log(linksInternos);
+
+  function scrollSuave(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollSuave);
+  });
+}
+
+initScroolSuave();
