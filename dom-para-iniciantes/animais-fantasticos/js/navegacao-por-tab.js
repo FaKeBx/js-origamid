ee35de6cd92112070ff691ee3a887cba66395c1b
Felipe kucharski <felipekb2000@furg.br>
2022-09-22 16:21:48 -0300

    parte de animacoes finalizada

--- dom-para-iniciantes/animais-fantasticos/js/navegacao-por-tab.js
+++ dom-para-iniciantes/animais-fantasticos/js/navegacao-por-tab.js
@@ -43,7 +43,6 @@ initAccordion();
 
 function initScroolSuave() {
   const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
-  console.log(linksInternos);
 
   function scrollSuave(e) {
     e.preventDefault();
@@ -62,3 +61,27 @@ function initScroolSuave() {
 }
 
 initScroolSuave();
+
+function initAnimaScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((item) => {
+        const sectionTop =
+          item.getBoundingClientRect().top - window.innerHeight * 0.6;
+        if (sectionTop < 0) {
+          item.classList.add("ativo");
+        } else {
+          item.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimaScroll();
