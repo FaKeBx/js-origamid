f0a58555c64fdbec23ed3ae33b12d12b40ad02bc
Felipe kucharski <felipekb2000@furg.br>
2022-09-28 08:34:40 -0300

    criando tooltip

--- dom-para-iniciantes/animais-fantasticos/js/modules/tooltip.js
+++ dom-para-iniciantes/animais-fantasticos/js/modules/tooltip.js
@@ -1,1 +1,22 @@
-export default function initTooltip() {}
+export default function initTooltip() {
+  const tooltips = document.querySelectorAll("[data-tooltip]");
+  console.log(tooltips);
+
+  tooltips.forEach((item) => {
+    item.addEventListener("mouseover", onMouseOver);
+  });
+
+  function onMouseOver(e) {
+    const tooltipBox = criarTooltip(this);
+    console.log(tooltipBox);
+  }
+
+  function criarTooltip(element) {
+    const tooltipBox = document.createElement("div");
+    const textBox = element.getAttribute("aria-label");
+    tooltipBox.classList.add("tooltip");
+    tooltipBox.innerText = textBox;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+  }
+}
